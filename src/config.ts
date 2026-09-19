68fcbcbe44e76dc1ff52373cce016f656f8689b5
AlberLC <alberlc@outlook.com>
2026-09-20 00:13:16 +0200

    Add secure API proxy

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,7 @@
-const API_ORIGIN = import.meta.env.VITE_API_ORIGIN ?? window.location.origin
-const API_BASE_URL = `${API_ORIGIN}/api`
+const API_BASE_URL = `${window.location.origin}/api`
 
 export const config = {
     API_BASE_URL,
-    API_ORIGIN,
     API_UPLOADS_ENDPOINT: `${API_BASE_URL}/files/uploads`,
     MAX_CONCURRENT_CHUNK_UPLOADS: 5
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
@@ -11,14 +12,23 @@ export default defineConfig({
         vue({
             template: { transformAssetUrls }
         }),
-        vueDevTools(),
+        // vueDevTools(),
         vuetify({
             autoImport: true
-        })
+        }),
+        basicSsl()
     ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5210',
+                changeOrigin: true
+            }
         }
     }
 })
